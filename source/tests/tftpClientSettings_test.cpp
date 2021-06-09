43c9a4ff1d05aeeb2c00a5e805a936e0b0bfa226
Vitaliy V. Shirinkin <vit_is_boss@mail.ru>
2021-06-09 20:06:34 +0500

    feat: New class ClientSettings in progress ...
    Issue #26

---
+++ source/tests/tftpClientSettings_test.cpp
@@ -0,0 +1,165 @@
+/*
+ * tftpClientSettings_test.cpp
+ *
+ *  Created on: 9 июн. 2021 г.
+ *      Author: svv
+ */
+
+
+
+
+#include "../tftpCommon.h"
+#include "../tftpClientSettings.h"
+#include "test.h"
+
+using namespace unit_tests;
+
+UNIT_TEST_SUITE_BEGIN(ClientSettings)
+
+//------------------------------------------------------------------------------
+
+/** \brief Helper class for unit-test access to Settings protected field
+ */
+class Settings_test: public tftp::ClientSettings
+{
+public:
+  Settings_test():
+    ClientSettings()
+  {
+    callback_log_ = std::bind(
+        & Settings_test::cout_log,
+        this,
+        std::placeholders::_1,
+        std::placeholders::_2);
+
+  };
+
+  //using ClientSettings::ClientSettings;
+  using ClientSettings::srv_addr_;
+  using ClientSettings::verb_;
+  using ClientSettings::file_local_;
+  using ClientSettings::file_remote_;
+  using ClientSettings::callback_log_;
+
+  void cout_log(tftp::LogLvl lvl, std::string_view msg) const
+  {
+    std::cout << "[DEBUG] " << tftp::to_string(lvl) << " " <<  msg << std::endl;
+  }
+};
+
+//------------------------------------------------------------------------------
+
+UNIT_TEST_CASE_BEGIN(parse_arg_cl, "Parse CMD client arguments")
+
+// 1
+START_ITER("default options");
+{
+  Settings_test b;
+  TEST_CHECK_TRUE (b.srv_addr_.family() == 0U);
+  TEST_CHECK_TRUE (b.srv_addr_.port() == tftp::constants::default_tftp_port);
+  TEST_CHECK_FALSE(b.verb_);
+  TEST_CHECK_TRUE (b.file_local_.size() == 0U);
+  TEST_CHECK_TRUE (b.file_remote_.size() == 0U);
+  //TEST_CHECK_TRUE (b.callback_log_ == nullptr);
+
+}
+
+// 2
+START_ITER("load options IPv4");
+{
+  const char * tst_args[]=
+  {
+    "./tftp-cl",
+    "10.0.0.202:6900",
+    "-l", "test_local.txt",
+    "-r", "test_remote.txt",
+    "-g",
+    "-v",
+    "-m", "netascii",
+    "-b", "1300",
+    "-t", "20",
+    "-w", "15",
+  };
+
+  Settings_test b;
+  TEST_CHECK_TRUE(b.load_options(sizeof(tst_args)/sizeof(tst_args[0]),
+                                 const_cast<char **>(tst_args)));
+  TEST_CHECK_TRUE(b.verb_);
+  TEST_CHECK_TRUE(b.file_local_ == "test_local.txt");
+  TEST_CHECK_TRUE(b.file_remote_ == "test_remote.txt");
+  TEST_CHECK_TRUE(b.blksize() == 1300);
+  TEST_CHECK_TRUE(b.timeout() == 20);
+  TEST_CHECK_TRUE(b.windowsize() == 15);
+  TEST_CHECK_TRUE(b.request_type() == tftp::SrvReq::read);
+  TEST_CHECK_TRUE(b.transfer_mode() == tftp::TransfMode::netascii);
+
+  /*
+  TEST_CHECK_TRUE(b.is_daemon);
+  TEST_CHECK_TRUE(b.use_syslog == 7);
+  TEST_CHECK_TRUE(b.local_base_.family() == 2U);
+  TEST_CHECK_TRUE(b.local_base_.port() == 7777U);
+  TEST_CHECK_TRUE(b.local_base_.as_in().sin_addr.s_addr == 16843009U);
+  TEST_CHECK_TRUE(b.local_base_.str() == "1.1.1.1:7777");
+  TEST_CHECK_TRUE(b.lib_dir == "/tmp/libs");
+  TEST_CHECK_TRUE(b.lib_name == "fbclient");
+  TEST_CHECK_TRUE(b.root_dir == "/mnt/tftp");
+  TEST_CHECK_TRUE(b.backup_dirs.size() == 3);
+  TEST_CHECK_TRUE(b.backup_dirs[0] == "/mnt/tst1");
+  TEST_CHECK_TRUE(b.backup_dirs[1] == "/mnt/tst2");
+  TEST_CHECK_TRUE(b.backup_dirs[2] == "/mnt/tst3");
+  TEST_CHECK_TRUE(b.db == "tester.fdb");
+  TEST_CHECK_TRUE(b.user == "SYSDBA");
+  TEST_CHECK_TRUE(b.pass == "masterkey");
+  TEST_CHECK_TRUE(b.role == "none");
+  TEST_CHECK_TRUE(b.dialect == 3U);
+  TEST_CHECK_TRUE(b.retransmit_count_ == 59U);
+  TEST_CHECK_TRUE(b.file_chown_user == "usr1");
+  TEST_CHECK_TRUE(b.file_chown_grp == "grp2");
+  TEST_CHECK_TRUE(b.file_chmod == 0766);
+  */
+}
+
+// 3
+START_ITER("load options IPv6");
+{
+  /*
+  const char * tst_args[]=
+  {
+    "./server-fw",
+    "--ip", "[fe80::1]:65000",
+    "--root-dir", "/mnt/tftp",
+  };
+
+  Settings_test b;
+  b.load_options(sizeof(tst_args)/sizeof(tst_args[0]),
+                 const_cast<char **>(tst_args));
+  TEST_CHECK_FALSE(b.is_daemon);
+  TEST_CHECK_TRUE(b.use_syslog == tftp::constants::default_tftp_syslog_lvl);
+  TEST_CHECK_TRUE(b.local_base_.family() == AF_INET6);
+  TEST_CHECK_TRUE(b.local_base_.port() == 65000U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 0] == 0xfeU);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 1] == 0x80U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 2] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 3] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 4] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 5] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 6] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 7] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 8] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[ 9] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[10] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[11] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[12] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[13] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[14] == 0x00U);
+  TEST_CHECK_TRUE(b.local_base_.as_in6().sin6_addr.__in6_u.__u6_addr8[15] == 0x01U);
+  TEST_CHECK_TRUE(b.local_base_.str() == "[fe80::1]:65000");
+  TEST_CHECK_TRUE(b.retransmit_count_ == tftp::constants::default_retransmit_count);
+  */
+}
+
+UNIT_TEST_CASE_END
+
+//------------------------------------------------------------------------------
+
+UNIT_TEST_SUITE_END
---
+++ source/tftpClientSettings.cpp
@@ -0,0 +1,202 @@
+/*
+ * tftpClientSettings.cpp
+ *
+ *  Created on: 9 июн. 2021 г.
+ *      Author: svv
+ */
+
+#include <getopt.h>
+
+#include "tftpClientSettings.h"
+
+namespace tftp
+{
+
+// -----------------------------------------------------------------------------
+
+ClientSettings::ClientSettings():
+    Options(),
+    srv_addr_{},
+    verb_{false},
+    file_local_{},
+    file_remote_{},
+    callback_log_{nullptr}
+{
+  srv_addr_.set_port(constants::default_tftp_port);
+}
+
+// -----------------------------------------------------------------------------
+
+ClientSettings::~ClientSettings()
+{
+}
+
+// -----------------------------------------------------------------------------
+
+auto ClientSettings::create() -> pClientSettings
+{
+  return std::make_unique<ClientSettings>(ClientSettings{});
+}
+
+//------------------------------------------------------------------------------
+
+bool ClientSettings::load_options(int argc, char * argv[])
+{
+  L_DBG("Start argument parse ("+std::to_string(argc)+")");
+
+  bool ret = true;
+
+  static const char *optString = "l:L:r:R:gGpPhHvVm:M:b:B:w:W:t:T:";
+
+  static const struct option longOpts[] =
+  {
+      { "local",      required_argument, NULL, 'l' }, // 0
+      { "remote",     required_argument, NULL, 'r' }, // 1
+      { "get",              no_argument, NULL, 'g' }, // 2
+      { "put",              no_argument, NULL, 'p' }, // 3
+      { "help",             no_argument, NULL, 'h' }, // 4
+      { "verbose",          no_argument, NULL, 'v' }, // 5
+      { "mode",       required_argument, NULL, 'm' }, // 6
+      { "blksize",    required_argument, NULL, 'b' }, // 7
+      { "windowsize", required_argument, NULL, 'w' }, // 8
+      { "timeout",    required_argument, NULL, 't' }, // 9
+      { "tsize",      required_argument, NULL,  0  }, // 10
+      { NULL,               no_argument, NULL,  0  }  // always last
+  };
+
+  optind=1;
+  while(argc > 1)
+  {
+    L_DBG("opt "+std::to_string(optind)+" '"+std::string{argv[optind]}+"'");
+    int longIndex;
+    int opt = getopt_long(argc, argv, optString, longOpts, & longIndex);
+    L_DBG("   opt "+std::to_string(opt));
+    if(opt == -1) break; // end parsing
+    switch(opt)
+    {
+      case 'l':
+      case 'L':
+        if(optarg) file_local_.assign(optarg);
+        break;
+      case 'r':
+      case 'R':
+        if(optarg) file_remote_.assign(optarg);
+        break;
+      case 'g':
+      case 'G':
+        request_type_ = SrvReq::read;
+        break;
+      case 'p':
+      case 'P':
+        request_type_ = SrvReq::write;
+        break;
+    case 'h':
+    case 'H':
+    case '?':
+      ret = false; // Help message cout
+      break;
+    case 'v':
+    case 'V':
+      verb_ = true;
+      break;
+    case 'm':
+    case 'M':
+      if(optarg)
+      {
+        std::string temp_str{optarg};
+        do_lower(temp_str);
+        if(temp_str == to_string(TransfMode::octet)) transfer_mode_ = TransfMode::octet;
+        else
+        if(temp_str == to_string(TransfMode::mail)) transfer_mode_ = TransfMode::mail;
+        else
+        if(temp_str == to_string(TransfMode::netascii)) transfer_mode_ = TransfMode::netascii;
+        else
+        if(temp_str == to_string(TransfMode::binary)) transfer_mode_ = TransfMode::binary;
+        else
+        transfer_mode_ = TransfMode::unknown;
+      }
+      break;
+
+    case 'b':
+    case 'B':
+      if(optarg)
+      {
+        int tmp_int = std::stol(optarg);
+        if(tmp_int > 0)
+        {
+          blksize_ = {true, tmp_int};
+        }
+      }
+      break;
+    case 'w':
+    case 'W':
+      if(optarg)
+      {
+        int tmp_int = std::stol(optarg);
+        if(tmp_int > 0)
+        {
+          windowsize_ = {true, tmp_int};
+        }
+      }
+      break;
+    case 't':
+    case 'T':
+      if(optarg)
+      {
+        int tmp_int = std::stol(optarg);
+        if(tmp_int > 0)
+        {
+          timeout_ = {true, tmp_int};
+        }
+      }
+      break;
+
+    case 0:
+      switch(longIndex)
+      {
+      case 10: // --tsize
+        if(optarg)
+        {
+          int tmp_int = std::stol(optarg);
+          if(tmp_int > 0)
+          {
+            tsize_ = {true, tmp_int};
+          }
+          else
+          { // only for WRQ use option with fill real file size (ignore arg value)
+            tsize_ = {true, 0};
+          }
+        }
+        break;
+
+      default:
+        L_ERR("Wrong long option index "+std::to_string(longIndex));
+        break;
+
+      } // switch for long options
+      break;
+
+    default:
+      L_ERR("Wrong short option index "+std::to_string(opt));
+      break;
+
+    } // switch for short options
+  }
+
+  L_DBG("Finish argument parse is "+(ret ? "SUCCESS" : "FAIL"));
+  return ret;
+}
+
+// -----------------------------------------------------------------------------
+
+void ClientSettings::log(LogLvl lvl, std::string_view msg) const
+{
+  if(callback_log_) callback_log_(lvl, msg);
+}
+
+// -----------------------------------------------------------------------------
+
+
+} // namespace tftp
+
+
