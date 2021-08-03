b39adbabc64f6e77831a176e98e4eec8c4ce0eae
Vitaliy V. Shirinkin <vit_is_boss@mail.ru>
2021-08-03 17:06:35 +0500

    In progress new conception with DataMgr* ... (u-t OK!)
    - all news in namespace "ext"
    
    Issue #26

--- source/tftpDataMgr.h
+++ source/tftpDataMgr.h
@@ -112,6 +112,102 @@ public:
 
 };
 
+//##############################################################################
+
+namespace ext
+{
+
+class DataMgr
+{
+protected:
+
+  // Processing info
+  Direction     dir_data_;  ///< Direction of data (read/write)
+  size_t        file_size_; ///< File size
+
+  fSetError     set_error_; ///< Callback error parsing to top level
+  //fLogMsg       log_msg_;   ///< Callback logging to top level
+
+  /** \brief Forward error to top level
+   *
+   *  Use property set_error_ as callback function, if property set
+   *  \param [in] e_cod Error code
+   *  \param [in] e_msg Error message
+   */
+  void set_error_if_first(
+      const uint16_t e_cod,
+      std::string_view e_msg) const;
+
+  /** Check requested value is md5 sum
+   *
+   *  Match by regex used 'regex_template_md5'
+   *  /return True if md5, else - false
+   */
+  bool match_md5(const std::string & val) const;
+
+public:
+
+  DataMgr();
+
+  /** \brief Destructor
+   */
+  virtual ~DataMgr();
+
+  /** Check active (opened stream) - abstract method
+   */
+  virtual bool active() const = 0;
+
+  /** \brief Initialize streams - abstract method
+   *
+   *  \param [in] sett Settings of tftp server
+   *  \param [in] cb_error Callback for error forward
+   *  \param [in] opt Options of tftp protocol
+   *  \return True on success, else - false
+   */
+  virtual bool init(
+      Base & sett,
+      fSetError cb_error,
+      const Options & opt) = 0;
+
+  /** \brief Write data stream operations - abstract method
+   *
+   *  \param [in] buf_begin Buffer begin iterator
+   *  \param [in] buf_end Buffer end iterator
+   *  \param [in] position Position received block (offset)
+   *  \return Processed size, -1 on error
+   */
+  virtual auto write(
+      SmBufEx::const_iterator buf_begin,
+      SmBufEx::const_iterator buf_end,
+      const size_t & position) -> ssize_t = 0;
+
+  /** \brief Read data stream operations - abstract method
+   *
+   *  \param [in] buf_begin Buffer begin iterator
+   *  \param [in] buf_end Buffer end iterator
+   *  \param [in] position Position transmitted block (offset)
+   *  \return Processed size, -1 on error
+   */
+  virtual auto read(
+      SmBufEx::iterator buf_begin,
+      SmBufEx::iterator buf_end,
+      const size_t & position) -> ssize_t = 0;
+
+  /**  Normal close all opened steams - abstract method
+   */
+  virtual void close() = 0;
+
+  /**  Cancel all opened steams - abstract method
+   *
+   *  If data will write, then need delete it:
+   *  - delete file for filesytem
+   *  - rollback transaction for DB
+   */
+  virtual void cancel() = 0;
+
+};
+
+}
 // -----------------------------------------------------------------------------
 
 } // namespace tftp
